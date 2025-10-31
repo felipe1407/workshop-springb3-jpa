0205c8bf1fb119e0650e4df38577aaf69b677b4d
Felipe Martins <felipecardoso1508@gmail.com>
2025-10-31 18:11:56 -0300

    Order, Instant, ISO8601

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.educandoweb.course.config;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration  //classe específica de Configuração
@@ -16,12 +20,20 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+
     @Override
     public void run(String... args) throws Exception {
 
-        User u1 = new User(null, "Maria brwon", "mar@gmail.com", "98888898", "123456");
+        User u1 = new User(null, "Maria brown", "mar@gmail.com", "98888898", "123456");
         User u2 = new User(null, " Felipe Red", "fer@gmail.com", "99988898", "1234567");
 
+
+        Order o1 = new Order(null, Instant.parse("2025-06-20T19:53:07Z"),u1);
+        Order o2 = new Order(null, Instant.parse("2025-07-21T03:42:10z"), u2);
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2));
     }
 }
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,15 +1,22 @@
 package com.educandoweb.course.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
 
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -18,6 +25,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+    //coleção só colocamos o get
     public User(){
 
     }
@@ -70,6 +81,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -81,6 +96,9 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
+
 
 }
 
